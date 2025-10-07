70480fa1490d1222f48a75cc3a53bfd4c01fb314
Daria Ternovska <119539125+dariaternovska@users.noreply.github.com>
2025-10-07 19:38:52 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        sum3(2, 4, 6);
+
+
+    }
+
+    public static void sum3(int a, int b, int c) {
+        int res = a + b + c;
+        System.out.println("sum: " + res);
+    }
+}
+
